bace56b9041fea78f3cded6e874dfed0b3c9cdd2
Jonas Freißmann <j.freissmann@web.de>
2024-06-06 08:58:28 +0200

    Expand docstrings of all methods of Legend class.

--- src/mpllegends/legend.py
+++ src/mpllegends/legend.py
@@ -9,6 +9,50 @@ class Legend():
                  fontfamily='sans-serif', fontstyle='normal',
                  fontweight='normal', fontsize='medium', borderpad=0.4,
                  labelspacing=0.5, columnspacing=2.0):
+        """
+        Initialize `Legend` instance.
+
+        Most parameters are so called `run time configuration parameters` of
+        matplotlib. See https://matplotlib.org/stable/users/explain/customizing.html#the-default-matplotlibrc-file
+        for a describtion of them.
+
+        Parameters
+        ----------
+
+        ncol : int
+            Number of columns of the legend. Entries are filled top to bottom
+            and left to right.
+
+        border : bool
+            Corresponds to `legend.frameon` rcParam
+
+        roundborder : bool
+            Corresponds to `legend.fancybox` rcParam
+
+        shadow : bool
+            Corresponds to `legend.shadow` rcParam
+
+        fontfamily : str
+            Corresponds to `font.family` rcParam
+
+        fontstyle : str
+            Corresponds to `font.style` rcParam
+
+        fontweight : str or int
+            Corresponds to `font.weight` rcParam
+
+        fontsize: str or int
+            Corresponds to `legend.fontsize` rcParam
+
+        borderpad: float
+            Corresponds to `legend.borderpad` rcParam
+
+        labelspacing: float
+            Corresponds to `legend.labelspacing` rcParam
+
+        columnspacing: float
+            Corresponds to `legend.columnspacing` rcParam
+        """
         self.entries = []
         self.fig = None
         self.ax = None
@@ -32,11 +76,25 @@ class Legend():
         Add entry to legend.
 
         Entries will be displayed in the order they are added.
+
+        Parameters
+        ----------
+
+        entry : any mpllengends.entries.Entry subclass
+            The entry to be added to the `Legend`.
         """
         self.entries.append(entry.obj)
 
     def create(self, figsize=None):
-        """Create matplotlib legend instance."""
+        """
+        Create matplotlib legend instance.
+        
+        Parameters
+        ----------
+
+        figsize : sequence of int or sequence of float
+            Size of figure in inches. First is width and second is height.
+        """
         with mpl.rc_context(self.rcParams):
             if figsize is not None:
                 self.fig, self.ax = plt.subplots(figsize=figsize)
@@ -71,7 +129,16 @@ class Legend():
             plt.show()
 
     def save(self, filepath):
-        """Save file of the legend."""
+        """
+        Save file of the legend.
+
+        Parameters
+        ----------
+
+        filepath : str
+            Absolute or relative path to the file that should be created. See
+            matplotlib documentation for supported file types.
+        """
         if self.legend is None:
             print('No legend exists that can be shown. Use `create` method.')
         else:
